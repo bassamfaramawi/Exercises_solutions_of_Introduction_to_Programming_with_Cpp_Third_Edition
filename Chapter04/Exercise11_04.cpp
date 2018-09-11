030cf0acd434a1efc49cfab10d9167492c7b8aeb
Bassam Faramawi email = tiodaronzi3@yahoo.com <Bassam>
2018-09-12 01:30:28 +0300

    Exercises solutions of Chapter 04

---
+++ Chapter04/Exercise11_04.cpp
@@ -0,0 +1,38 @@
+/*
+ *4.11 (Convert an uppercase letter to lowercase) Write a program that prompts 
+ * the user to enter an uppercase letter and converts it to a lowercase letter.
+ */
+
+/* 
+ * File:   Exercise11_04.cpp
+ * Author: BASSAM FARAMAWI / tiodaronzi3@yahoo.com
+ *
+ * Created on September 11, 2018, 6:31 PM
+ */
+
+#include <iostream> // For cin & cout functions
+
+
+using namespace std;
+
+int main() {
+    // Prompt the user to enter an uppercase letter
+    cout << "Enter an uppercase letter: ";
+    char upperLetter;
+    cin >> upperLetter;
+    
+    // Check if the character isn't alphabetic
+    if(!isalpha(upperLetter))
+        cout << "The input is  invalid";
+    
+    // Otherwise change case
+    else {
+        // Switch case
+        char lowerLetter = tolower(upperLetter);
+        
+        // Display the result
+        cout << "The lowercase is " << lowerLetter << endl;
+    }
+    
+    return 0;
+}
